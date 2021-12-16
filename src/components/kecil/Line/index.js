774c5bdc15ada4ad2b24f42224daceb18ae715de
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-12-16 11:21:53 +0700

    fix bug component line

--- src/components/kecil/Line/index.js
+++ src/components/kecil/Line/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 const Line = () => {
-  return <View style={styles.line} />;
+  return <View style={styles.line}></View>;
 };
 
 export default Line;
 
 const styles = StyleSheet.create({
   line: {
-    borderTopWidth: 1,
-    borderTopColor: 'red',
+    borderBottomColor: 'black',
+    width: '100%',
+    borderBottomWidth: 1,
   },
 });
